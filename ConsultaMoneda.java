f98e2ee10d9290b4faff648607ef99913ef85294
Susanayuque <susanayuque27@gmail.com>
2025-06-01 19:00:58 +1000

    primeraEtapa

---
+++ ConsultaMoneda.java
@@ -0,0 +1,32 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaMoneda {
+
+    public Moneda encontrarMoneda(String convertirFrom, String convertirTo) throws IOException, InterruptedException {
+
+        String direccion = "https://v6.exchangerate-api.com/v6/b3657e56053dc65c43800436/pair/"+convertirFrom+"/"+convertirTo;
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(direccion))
+                .build();
+
+        try {
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            String json = response.body();
+            return new Gson().fromJson(json, Moneda.class);
+        }
+
+        catch (Exception e){
+
+            throw new RuntimeException("La moneda no se encontro");
+        }
+    }
+}
---
+++ Principal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Sea bienvenido al conversor de moneda =)");
+        System.out.println("Elija un apcion valida");
+        System.out.println("1) Dolar =>> Peso Argentino"+"\n"
+                +"2) Soles =>> Dolares Australianos"+"\n"
+                +"3) Dolares Australianos =>> dolar"+"\n"
+                +"4) Dolares Australianos =>> soles"+"\n"
+                +"5) Dolar =>> Dolares Australianos"+"\n"
+                +"6) Dolar =>> Peso Colombiano"+"\n"
+                +"7) Salir");
+        Scanner scanner = new Scanner(System.in);
+    }
+}
