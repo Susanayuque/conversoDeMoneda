dd97cccf655e6fb4c8fb3cead59a9791c38180b4
Susanayuque <susanayuque27@gmail.com>
2025-06-01 21:42:45 +1000

    SegundaEtapa

--- ConversorDeMoneda.java
+++ ConversorDeMoneda.java
@@ -1,3 +1,18 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class ConversorDeMoneda {
 
+    public static void convertirMoneda(String monedaBase, String monedaTarget, ConsultaMoneda consultaMoneda, Scanner lectura) throws IOException, InterruptedException {
+
+        double cantidad;
+        double cantidadConvertida;
+
+        Moneda moneda = consultaMoneda.encontrarMoneda(monedaBase,monedaTarget);
+        System.out.println("1"+monedaBase+"="+moneda.conversion_rate()+" "+monedaTarget);
+        System.out.println("Indique la cantidad que desea convertir");
+        cantidad = lectura.nextDouble();
+        cantidadConvertida = cantidad*moneda.conversion_rate();
+        System.out.println("La cantidad total convertida es" +cantidadConvertida+" "+monedaTarget);
+    }
 }
